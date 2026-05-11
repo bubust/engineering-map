afa8de534850610ef5e17d71931f1f2578db3b76
bubust <bubust@gmail.com>
2026-05-11 10:00:38 +0800

    Rewrite recording panel: non-blocking, cancel support, low-bitrate, badge mode

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2793115946" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3439365046" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
