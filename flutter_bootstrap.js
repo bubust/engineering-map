3736b0e15ebe2096f0af446314e8a6fe17b5f9d6
bubust <bubust@gmail.com>
2026-05-12 13:15:32 +0800

    Fix recording (no capture attr), input panel ±1 flanking, block panel clean layout

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2566374254" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "933584936" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
